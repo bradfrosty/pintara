7a1178e22930f734554c855d3f36e96ed9fca131
Bradley Frost <bfrost@umich.edu>
2015-12-31 04:36:38 +0000

    Begin route for uploading a picture

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,9 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello!"
+@app.route("/paintings")
+def createNewPainting():
+    return "Create a new painting"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
