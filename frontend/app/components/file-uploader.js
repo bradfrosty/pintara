7b2cac6f1f54f76afa470de8569aec0409bdec4f
Bradley Frost <bfrost@umich.edu>
2016-02-25 00:53:45 -0500

    Generalize fileuploader component

--- frontend/app/components/file-uploader.js
+++ frontend/app/components/file-uploader.js
@@ -2,30 +2,32 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
     tagName: 'div',
-    classNameBindings: 'isDragging isDisabled:is-disabled'.w(),
+    classNameBindings: 'isDisabled:is-disabled'.w(),
     attributeBindings: 'data-uploader'.w(),
     'data-uploader': 'true',
     isDisabled: false,
+    dragCount: 0,
+    isDragging: Ember.computed('dragCount', function() {
+        return this.get('dragCount') > 0;
+    }),
+    displayText: "Drag here",
 
-
-    dragOver: function(event){
+    dragOver: function(event) {
         // this is needed to avoid the default behaviour from the browser
         event.preventDefault();
     },
 
-    dragEnter: function(event){
+    dragEnter: function(event) {
         event.preventDefault();
-        console.log('entering');
-        this.set('isDragging', true);
+        this.incrementProperty('dragCount');
     },
 
-    dragLeave: function(event){
+    dragLeave: function(event) {
         event.preventDefault();
-        console.log('leaving');
-        this.set('isDragging', false);
+        this.decrementProperty('dragCount');
     },
 
-    drop: function(event){
+    drop: function(event) {
         var file;
 
         if(!this.get('isDisabled')){
@@ -34,7 +36,7 @@ export default Ember.Component.extend({
 
             // only 1 file for now
             file = event.dataTransfer.files[0];
-            this.set('isDisabled', true);
+            // this.set('isDisabled', true);
             this.sendAction('dropFile', file);
         } else{
             console.error('you can only upload on file at the time');
